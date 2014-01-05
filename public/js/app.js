8425e9595bb5205f313f8d9143b2710c963523ea
individual11 <david@individual11.com>
2014-01-05 01:52:46 -0500

    got figlet working

--- public/js/app.js
+++ public/js/app.js
@@ -11,4 +11,27 @@ $(function(){
 		speed:500
 	});
 
+	/* FIGLET */
+	var $figletSelect	=	$('#figlet-select');
+	var $figletInput	=	$('#figlet-input');
+	var $figletDisplay	=	$('#figlet-display');
+
+	function updateFiglet(e){
+		figlet($figletInput.val(), $figletSelect.val(), function(err, text) {
+			if (err) {
+				console.log('something went wrong...');
+				console.dir(err);
+				return;
+			}
+			$figletDisplay.text(text);
+			//console.log(text);
+		});
+	}
+
+	$figletSelect.change(updateFiglet);
+	$('#figlet-input:text').on('input',updateFiglet);
+
+	updateFiglet();
+
+
 });
